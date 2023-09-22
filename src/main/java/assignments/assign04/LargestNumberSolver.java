52c5bf8d23906e50b03e3cd3fe07cf6d3d884552
Endoki <stewart@therussells.net>
2023-09-21 21:35:39 -0600

    assign04 LargestNumberSolver.

--- src/main/java/assignments/assign04/LargestNumberSolver.java
+++ src/main/java/assignments/assign04/LargestNumberSolver.java
@@ -5,10 +5,27 @@ import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.*;
 
+/*
+ * Assignment: 4
+ * @Version: 09/20/2023
+ * @Author: Adarsh Sreeram & Stewart Russell
+ */
 public class LargestNumberSolver {
+    /*
+     * customComparator: Finds the largest number by comparing two numbers
+     * @return: returns the largest number in the list
+     */
+    public static Comparator<Integer> customComparator = (Integer x, Integer y) -> {
+        String xy = x.toString() + y.toString();
+        String yx = y.toString() + x.toString();
+        return yx.compareTo(xy);
+    };
 
-    public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp)
-    {
+    /*
+     * @param: arr: an array of integers, cmp: a comparator
+     * @return: void
+     */
+    public static <T> void insertionSort(T[] arr, Comparator<? super T> cmp) {
         for (int i = 1; i < arr.length; i++) {
             T key = arr[i];
             int j = i - 1;
@@ -21,15 +38,15 @@ public class LargestNumberSolver {
         }
     }
 
-    public static BigInteger findLargestNumber(Integer[] arr)
-    {
+    /*
+     * findLargestNumber method that finds the largest number in an array
+     * @param: arr: an array of integers
+     * @return: BigInteger that is the largest number in an array
+     */
+    public static BigInteger findLargestNumber(Integer[] arr) {
         if (arr.length == 0) return BigInteger.ZERO;
 
-        insertionSort(arr, (Integer x, Integer y) -> {
-            String xy = x.toString() + y.toString();
-            String yx = y.toString() + x.toString();
-            return yx.compareTo(xy);
-        });
+        insertionSort(arr, customComparator);
 
         StringBuilder sb = new StringBuilder();
         for (Integer num : arr) {
@@ -39,20 +56,41 @@ public class LargestNumberSolver {
         return new BigInteger(sb.toString());
     }
 
+    /*
+     * findLargestNumber method that finds the largest number in an array
+     * @param: arr: an array of integers
+     * @return: bigNum(BigInteger)
+     * @throws: OutOfRangeException
+     */
     public static int findLargestInt(Integer[] arr) throws OutOfRangeException
     {
         BigInteger bigNum = findLargestNumber(arr);
-        if(bigNum.bitCount() > 32) throw new OutOfRangeException("int");
+        if(bigNum.compareTo(new BigInteger(String.valueOf(Integer.MAX_VALUE))) > 0 ||
+                bigNum.compareTo(new BigInteger(String.valueOf(Integer.MIN_VALUE))) < 0) {
+            throw new OutOfRangeException("int");
+        }
+
         return bigNum.intValue();
     }
-
+    /*
+     * findLargestLong method that finds the largest number in an array
+     * @param: arr: an array of integers
+     * @return: bigNum(BigInteger) long value
+     * @throws: OutOfRangeException
+     */
     public static long findLargestLong(Integer[] arr) throws OutOfRangeException
     {
         BigInteger bigNum = findLargestNumber(arr);
         if(bigNum.bitCount() > 64) throw new OutOfRangeException("long");
-        return bigNum.intValue();
-    }
+        return bigNum.longValue();
 
+    }
+    /*
+     * findKthLargest method that finds the kth largest number in an array
+     * @param: list: a list of arrays of integers, k: an integer
+     * @return: an array of integers
+     * @throws: IllegalArgumentException if k is not a valid position in the list.
+     */
     public static Integer[] findKthLargest(List<Integer[]> list, int k) throws IllegalArgumentException {
         if (k < 0 || k >= list.size()) {
             throw new IllegalArgumentException("k is not a valid position in the list.");
@@ -63,24 +101,37 @@ public class LargestNumberSolver {
             BigInteger num2 = findLargestNumber(arr2);
             return num2.compareTo(num1);
         };
-
         Integer[][] sortList = list.toArray(new Integer[list.size()][]);
         insertionSort(sortList, customComparator);
 
         return sortList[k];
     }
 
+    /*
+     * sum method that finds the sum of all the largest numbers in a list of arrays
+     * @param: list: a list of arrays of integers
+     * @return: totalSum which is a bigInteger
+     */
     public static BigInteger sum(List<Integer[]> list)
     {
-        BigInteger totalSum = BigInteger.ZERO;
+        List<Integer[]> deepCopyList = new ArrayList<>();
         for (Integer[] arr : list) {
+            Integer[] arrCopy = Arrays.copyOf(arr, arr.length);
+            deepCopyList.add(arrCopy);
+        }
+        BigInteger totalSum = BigInteger.ZERO;
+        for (Integer[] arr : deepCopyList) {
             BigInteger largestNumber = findLargestNumber(arr);
             totalSum = totalSum.add(largestNumber);
         }
-
         return totalSum;
     }
 
+    /*
+     * readFile method that reads a file and returns a list of arrays of integers
+     * @param: filename: the name of the file to be read
+     * @return: a list of arrays of integers
+     */
     public static List<Integer[]> readFile(String filename)
     {
         File src = new File(filename);
@@ -98,8 +149,6 @@ public class LargestNumberSolver {
                 }
                 output.add(arr);
             }
-
-            sc.close();
 
             return output;
         } catch(FileNotFoundException e)
--- src/test/java/assignments/assign04/LargestNumberSolverTest.java
+++ src/test/java/assignments/assign04/LargestNumberSolverTest.java
@@ -10,9 +10,15 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/*
+ * Assignment: 4
+ * @Author: Adarsh Sreeram & Stewart Russell
+ * @Version: 09/21/2023
+ */
 class LargestNumberSolverTest {
-
+    /*
+     * Instance variables: intArr, longArr, bigArr
+     */
     Integer[] intArr;
     Integer[] longArr;
     Integer[] bigArr;
@@ -24,31 +30,33 @@ class LargestNumberSolverTest {
         longArr = new Integer[]{32, 32, 82, 99, 91, 11, 817, 93};
         bigArr = new Integer[]{410, 21, 93, 80, 69, 379, 20, 60, 432, 13, 72, 62, 70, 83, 9, 3, 14, 11, 62, 55, 34, 83, 80, 99, 56, 25, 79, 51, 51, 70, 79, 20, 34, 67, 40, 51, 41, 94, 89, 116, 874, 554, 137, 371, 17, 77, 97, 58, 83, 97, 26, 17, 54, 96, 33};
     }
-
+    /*
+     * findLargestNumberTest method that checks if the largest number in an array is correct
+     */
     @Test
     void findLargestNumberTest()
     {
         assertEquals(new BigInteger("8536231"), LargestNumberSolver.findLargestNumber(intArr));
     }
-
+    //findLargestIntTest that checks if the largest number in an array is correct
     @Test
     void findLargestIntTest()
     {
         assertEquals(8536231, LargestNumberSolver.findLargestInt(intArr));
     }
-
+    //findLargestIntExceptionTest that checks if the right exception is thrown
     @Test
     void findLargestIntExceptionTest()
     {
         assertThrows(OutOfRangeException.class, () -> LargestNumberSolver.findLargestInt(bigArr));
     }
-
+    //findLargestLongTest that checks if the largest long number in an array is correct
     @Test
     void findLargestLongTest()
     {
         assertEquals(8536231L, LargestNumberSolver.findLargestLong(intArr));
     }
-
+    //findLargestLongExceptionTest that checks if the right exception is thrown
     @Test
     void findLargestLongExceptionTest()
     {}
@@ -60,7 +68,7 @@ class LargestNumberSolverTest {
             sb.append(item.toString() +(item.equals(arr[arr.length - 1]) ? "" : ", "));
         System.out.println(sb);
     }
-
+    //testFindKthLargest test method that checks if the kth largest number in a list of arrays is correct
     @Test
     void testFindKthLargest() {
         List<Integer[]> sampleList = new ArrayList<>();
@@ -71,18 +79,19 @@ class LargestNumberSolverTest {
         assertTrue(Arrays.equals(LargestNumberSolver.findKthLargest(sampleList, 1), sampleList.get(1))); // Second largest
         assertThrows(IllegalArgumentException.class, () -> LargestNumberSolver.findKthLargest(sampleList, 2)); // Third largest DNE
     }
-
+    //testBigSum test method that checks if the sum of the largest numbers in a list of arrays is correct
     @Test
     void testBigSum() {
         // Normal cases
         BigInteger sum = LargestNumberSolver.sum(Arrays.asList(new Integer[][]{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}}));
         assert sum.equals(BigInteger.valueOf(54321 + 987610)) : "Expected: " + (54321 + 987610) + "\nResult: " + sum;
-    }
 
+    }
+    //readFileTest method that checks if the file is read correctly
     @Test
     void readFileTest()
     {
-        String path = new File("drop/integers.txt").getAbsolutePath();
+        String path = new File("integers.txt").getAbsolutePath();
         List<Integer[]> list = LargestNumberSolver.readFile(path);
         assertEquals(list.get(0)[0], 410);
     }
